b863c40e7036945b3372045146536103ca0bb4fb
Francis KANTE <42733388+fkante@users.noreply.github.com>
2021-11-04 09:20:33 +0100

    hardhat example file

---
+++ hardhat.config.js
@@ -0,0 +1,14 @@
+require("@nomiclabs/hardhat-waffle");
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: "0.8.0",
+  networks: {
+    rinkeby: {
+      url: 'ACCOUNT_URL',
+      accounts: ['ACCOUNT_PRIVATE_KEY'],
+    },
+  },
+};
